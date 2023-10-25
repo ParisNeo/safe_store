22cd62673924482167905bc7dc30aa921530906c
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2023-10-25 17:39:24 +0200

    Update generic_data_loader.py

--- safe_store/generic_data_loader.py
+++ safe_store/generic_data_loader.py
@@ -56,7 +56,7 @@ class GenericDataLoader:
     def read_json_file(file_path):
         import json
         with open(file_path, 'r') as file:
-            data = json.load(file)
+            data = str(json.load(file))
         return data
     
     @staticmethod
