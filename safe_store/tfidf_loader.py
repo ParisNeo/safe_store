d31df5d7f5dc73be5ca99cc114e4adab628dc3b8
Saifeddine ALOUI <aloui.seifeddine@gmail.com>
2024-05-03 14:46:45 +0200

    Update tfidf_loader.py

--- safe_store/tfidf_loader.py
+++ safe_store/tfidf_loader.py
@@ -6,8 +6,8 @@ class TFIDFLoader:
     def create_vectorizer_from_dict(tfidf_info):
         vectorizer = TfidfVectorizer(**tfidf_info['params'])
         vectorizer.ngram_range = tuple(vectorizer.ngram_range)
-        vectorizer.vocabulary_ = tfidf_info['vocabulary']
-        vectorizer.idf_ = tfidf_info['idf_values']
+        vectorizer.vocab = tfidf_info['vocabulary']
+        vectorizer.idf = tfidf_info['idf_values']
         dt = vectorizer.dtype[8:-2]
         vectorizer.dtype = eval(dt)
         return vectorizer
@@ -15,8 +15,8 @@ class TFIDFLoader:
     @staticmethod
     def create_dict_from_vectorizer(vectorizer):
         tfidf_info = {
-            "vocabulary": vectorizer.vocabulary_,
-            "idf_values": vectorizer.idf_,# dict(zip(vectorizer.get_feature_names(), )),
+            "vocabulary": vectorizer.vocab,
+            "idf_values": vectorizer.idf,# dict(zip(vectorizer.get_feature_names(), )),
             "params": vectorizer.get_params()
         }
         tfidf_info["params"]["dtype"]=str(tfidf_info["params"]["dtype"])
